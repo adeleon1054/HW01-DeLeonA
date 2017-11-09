59a673c4585baff843a7f87c260ce85e45430b2a
Asdruval De Leon <adeleon1054@gmail.com>
2017-11-09 18:01:31 -0500

    detail view completed -> AD

--- HW01-DeLeonA/StateParkTableViewController.m
+++ HW01-DeLeonA/StateParkTableViewController.m
@@ -41,15 +41,16 @@
     return 0;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
     // Configure the cell...
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
